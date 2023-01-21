14f1bcedf99a689ad0bc5dfcf328cc4fa8c1dd38
SyamSrikar <srampall1@gitam.in>
2023-01-21 10:46:22 +0530

    theme responsive

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -5,14 +5,18 @@ import '../App.css';
 
 function Themes(props) {
   return (
-    <>
-        <Flex  alignItems={'center'} paddingX={props.value?'10px':'50px'} backgroundColor={'#020710'} color={'white'} flexWrap={'wrap'} width={'100%'}>
+    <>  
+        ({props.value && <Flex flexDir={'column'} justifyContent={'center'} marginTop={'30px'} alignItems={'center'} paddingX={props.value?'10px':'50px'} backgroundColor={'#020710'} color={'white'} flexWrap={'wrap'}>
+            <Flex paddingY={'10px'} className='heading'><p>THEMES</p></Flex>
+            <Flex padding={'10px 0px 10px 10px'} className='theme' flexWrap={'wrap'}><p>Shorefest 2023 believes that youth are the future leaders of the world and to tackle the problems of the present and future we have our theme as the Sustainable Development Goals set by the United Nations-theme content on the website.</p></Flex>
+            <Flex flexGrow={'1'} width={'100%'} justifyContent={'center'}><ImageAlign val={props.value}/></Flex>
+        </Flex>} || {!props.value && <Flex  alignItems={'center'} paddingX={props.value?'10px':'50px'} backgroundColor={'#020710'} color={'white'} flexWrap={'wrap'} width={'100%'}>
             <Flex width={'60%'} justifyContent={'center'}><ImageAlign val={props.value}/></Flex>
             <Flex flexDir={'column'} width={'40%'} flexWrap={'wrap'}>
               <Flex paddingY={'10px'} className='heading'><p>THEMES</p></Flex>
               <Flex padding={'10px 0px 10px 10px'} className='theme' flexWrap={'wrap'}><p>Shorefest 2023 believes that youth are the future leaders of the world and to tackle the problems of the present and future we have our theme as the Sustainable Development Goals set by the United Nations-theme content on the website.</p></Flex>
             </Flex>
-        </Flex>
+        </Flex>})
 
     </>
   )
