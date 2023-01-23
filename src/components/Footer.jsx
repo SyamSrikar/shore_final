f2fd2fc6a092f22dbc0afabaaa99b697bab73d3c
syamsrikar <srampall1@gitam.in>
2023-01-24 00:54:31 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = (props) => {
   const link_style={justifyContent:'space-between' ,marginTop:'17px' ,alignItems:'center',fontSize:'25px',cursor:'pointer' ,width:'155px'};
   return (
     <Flex backgroundColor={'#020710'} padding={'40px'} paddingTop={'0px'} color={'white'} flexWrap={'wrap'} justifyContent={'space-around'}> 
-        {!props.value && <Flex height={'250px'} marginBottom={props.value?'50px':'20px'}> <img src='../images/logo.png' alt='LOGO'></img> </Flex>}
+        {!props.value && <Flex height={'250px'} marginBottom={props.value?'50px':'20px'}> <img src='./images/logo.png' alt='LOGO'></img> </Flex>}
         <Flex flexDir={'column'} justifyContent={'flex-start'} marginBottom={props.value?'50px':'0px'}  >
             <p className='footer-heading' ><b>Visakhapatnam Campus</b></p>
             <br></br>
@@ -23,24 +23,24 @@ const Footer = (props) => {
              <Flex justifyContent={'center'} className='footer-heading '><b>Social Media Links</b></Flex>
              <Flex flexDir={'column'} marginLeft={'40px'} width={'60%'} justifyContent={'center'} >
                 <Flex style={link_style} onClick={()=>window.location.href = "https://www.instagram.com/shore.gitam/"}>  
-                  <img src='../images/instagram.png' width={'35px'} height={'35px'}></img>
+                  <img src='./images/instagram.png' width={'35px'} height={'35px'}></img>
                   <p className='footer-link'>Instagram</p>
                 </Flex>
                 <Flex style={link_style} onClick={()=>window.location.href = "https://www.facebook.com/profile.php?id=100088443708932&mibextid=LQQJ4d"}>
-                  <img src='../images/facebook.png' width={'35px'} height={'35px'}></img>
+                  <img src='./images/facebook.png' width={'35px'} height={'35px'}></img>
                   <p className='footer-link'>Facebook</p>
                 </Flex>
                 <Flex style={link_style} onClick={()=>window.location.href = "https://www.linkedin.com/company/shore-gitam-deemed-to-be-university/"}>
-                  <img src='../images/linkedin.png' width={'35px'} height={'35px'}></img>
+                  <img src='./images/linkedin.png' width={'35px'} height={'35px'}></img>
                   <p className='footer-link'>LinkedIn</p>
                 </Flex>
                 <Flex style={link_style} onClick={()=>window.location.href = "https://www.snapchat.com/add/shore.gitam?share_id=4NQqf4oGTgy7i8JkEVCrYw&locale=en_IN&sid=489faf2175d04d3d9092db22048b16f9"}>
-                  <img src='../images/snapchat.png' width={'35px'} height={'35px'}></img>
+                  <img src='./images/snapchat.png' width={'35px'} height={'35px'}></img>
                   <p className='footer-link'>Snapchat</p>
                 </Flex>
              </Flex>
         </Flex>
-        {props.value && <Flex height={'250px'} marginBottom={props.value?'50px':'20px'}> <img src='../images/logo.png' alt='LOGO'></img> </Flex>}
+        {props.value && <Flex height={'250px'} marginBottom={props.value?'50px':'20px'}> <img src='./images/logo.png' alt='LOGO'></img> </Flex>}
     </Flex>
   )
 }
